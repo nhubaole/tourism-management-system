6d4ee7f524817a5dfccc11a11ea0098168dcabfe
nhubaole <okamisameko7123@gmail.com>
2023-05-21 11:53:22 +0700

    update database + bind data in ScheduleWindow

--- TourismManagementSystem/ViewModel/ScheduleVM.cs
+++ TourismManagementSystem/ViewModel/ScheduleVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,21 @@ namespace TourismManagementSystem.ViewModel
     {
         private static TUYEN currTour;
         private string _SoNgayDem;
+        private ObservableCollection<NgayTour> listNgay;
 
         public ScheduleVM()
         {
             SoNgayDem = CurrTour.SONGAY + " ngày " + CurrTour.SODEM + " đêm";
+            listNgay = new ObservableCollection<NgayTour>();
+            for(int i = 1; i <= CurrTour.SONGAY; i++)
+            {
+                NgayTour ngayTour = new NgayTour("Ngày " + i);
+                foreach(var item in DataProvider.Ins.DB.LICHTRINHs)
+                {
+                    ngayTour.ListLichTrinh.Add(item);
+                }
+                ListNgay.Add(ngayTour);
+            }
         }
 
         public static TUYEN CurrTour { 
@@ -26,5 +38,22 @@ namespace TourismManagementSystem.ViewModel
         }
 
         public string SoNgayDem { get => _SoNgayDem; set { _SoNgayDem = value; OnPropertyChanged(); }  }
+
+        public ObservableCollection<NgayTour> ListNgay { get => listNgay; set { listNgay = value; OnPropertyChanged(); } }
+    }
+
+    class NgayTour : BaseViewModel
+    {
+        private string _NgayThu;
+        private ObservableCollection<LICHTRINH> _ListLichTrinh;
+
+        public NgayTour(string ngayThu = "")
+        {
+            NgayThu = ngayThu;
+            ListLichTrinh = new ObservableCollection<LICHTRINH>();
+        }
+
+        public string NgayThu { get => _NgayThu; set { _NgayThu = value; OnPropertyChanged(); }  }
+        public ObservableCollection<LICHTRINH> ListLichTrinh { get => _ListLichTrinh; set { _ListLichTrinh = value; OnPropertyChanged(); }  }
     }
 }
