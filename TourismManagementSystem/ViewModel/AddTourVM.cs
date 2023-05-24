1439facb4350732beb9bee43d43f6d70f369b33c
nhubaole <okamisameko7123@gmail.com>
2023-05-24 12:54:47 +0700

    add: edit, delete tour feature

--- TourismManagementSystem/ViewModel/AddTourVM.cs
+++ TourismManagementSystem/ViewModel/AddTourVM.cs
@@ -13,18 +13,37 @@ namespace TourismManagementSystem.ViewModel
 {
     internal class AddTourVM : BaseViewModel
     {
-        private TUYEN newTour = new TUYEN();
+        private TUYEN newTour;
         public ObservableCollection<DIADIEM> ListDiaDiem { get; set; }
         public ObservableCollection<LOAITUYEN> ListLoaiTuyen { get; set; }
         private string _ToolTipText;
+
+        private static TUYEN _EditSelectedTuyen;
+        private static int _IsEdit = 0;
+
+        private string _Title;
+        private string _BtnText;
 
         public AddTourVM()
         {
             ToolTipText = "Vui lòng nhập đủ các trường thông tin bắt buộc";
             ListDiaDiem = new ObservableCollection<DIADIEM>(DataProvider.Ins.DB.DIADIEMs);
             ListLoaiTuyen = new ObservableCollection<LOAITUYEN>(DataProvider.Ins.DB.LOAITUYENs);
-            Random random = new Random();
-            NewTour.MATUYEN = random.Next(0, 1000).ToString();
+            if (IsEdit == 0)
+            {
+                Title = "Thêm mới tuyến du lịch";
+                BtnText = "Thêm";
+                newTour = new TUYEN();
+                Random random = new Random();
+                NewTour.MATUYEN = random.Next(0, 1000).ToString();
+            }
+            else
+            {
+                Title = "Cập nhật tuyến du lịch";
+                BtnText = "Cập nhật";
+                newTour = EditSelectedTuyen;
+            }
+            
             ScheduleOptionCommand = new RelayCommand<Window>((p) => { 
                 if (p == null)
                 {
@@ -65,9 +84,22 @@ namespace TourismManagementSystem.ViewModel
                         MessageBox.Show("Số ngày đêm không hợp lệ");
                         return;
                     }
-                    DataProvider.Ins.DB.TUYENs.Add(NewTour);
-                    DataProvider.Ins.DB.SaveChanges();
-                    MessageBox.Show("Thêm mới tuyến thành công!");
+                    if (IsEdit == 0)
+                    {
+                        DataProvider.Ins.DB.TUYENs.Add(NewTour);
+                        DataProvider.Ins.DB.SaveChanges();
+                        MessageBox.Show("Thêm mới tuyến thành công!");
+                    }
+                    else
+                    {
+                        var itemToUpdate = DataProvider.Ins.DB.TUYENs.FirstOrDefault(item => item.MATUYEN == NewTour.MATUYEN);
+                        if (itemToUpdate != null)
+                        {
+                            itemToUpdate = NewTour;
+                            DataProvider.Ins.DB.SaveChanges();
+                            MessageBox.Show("Cập nhật lịch trình thành công!");
+                        }
+                    }
                     addTourWindow.Close();
                 }
             });
@@ -78,5 +110,10 @@ namespace TourismManagementSystem.ViewModel
         public TUYEN NewTour { get => newTour; set { newTour = value; OnPropertyChanged(); } }
 
         public string ToolTipText { get => _ToolTipText; set { _ToolTipText = value; OnPropertyChanged(); }  }
+
+        public static TUYEN EditSelectedTuyen { get => _EditSelectedTuyen; set => _EditSelectedTuyen = value; }
+        public static int IsEdit { get => _IsEdit; set => _IsEdit = value; }
+        public string Title { get => _Title; set => _Title = value; }
+        public string BtnText { get => _BtnText; set => _BtnText = value; }
     }
 }
--- TourismManagementSystem/ViewModel/TourVM.cs
+++ TourismManagementSystem/ViewModel/TourVM.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using TourismManagementSystem.Model;
 using TourismManagementSystem.View;
@@ -18,13 +19,45 @@ namespace TourismManagementSystem.ViewModel
             ListTuyen = new ObservableCollection<TUYEN>(DataProvider.Ins.DB.TUYENs);
             AddTourCommand = new RelayCommand<object>((p) => true, (p) =>
             {
+                AddTourVM.IsEdit = 0;
                 AddTourWindow addTourWindow = new AddTourWindow();
                 addTourWindow.ShowDialog();
                 ListTuyen = new ObservableCollection<TUYEN>(DataProvider.Ins.DB.TUYENs);
             });
+            DeleteTourCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                TUYEN selectedTuyen = p as TUYEN;
+                if (MessageBox.Show("Bạn có chắc chắn muốn xóa tuyến du lịch này?", "Xác nhận", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    var ListLichTrinhOfDelTour = DataProvider.Ins.DB.LICHTRINHs.Where(lt => lt.TUYEN.MATUYEN == selectedTuyen.MATUYEN);
+
+                    foreach (var lichTrinh in ListLichTrinhOfDelTour)
+                    {
+                        lichTrinh.PHUONGTIENs.Clear();
+                        lichTrinh.KHACHSANs.Clear();
+                        lichTrinh.NHAHANGs.Clear();
+                        lichTrinh.DICHVUKHACs.Clear();
+                        DataProvider.Ins.DB.LICHTRINHs.Remove(lichTrinh);
+                    }
+                    DataProvider.Ins.DB.TUYENs.Remove(selectedTuyen);
+                    DataProvider.Ins.DB.SaveChanges();
+                    ListTuyen = new ObservableCollection<TUYEN>(DataProvider.Ins.DB.TUYENs);
+                    MessageBox.Show("Xóa tuyến du lịch thành công");
+                }
+            });
+            EditTourCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                TUYEN selectedTuyen = p as TUYEN;
+                AddTourVM.EditSelectedTuyen = selectedTuyen;
+                AddTourVM.IsEdit = 1;
+                AddTourWindow addTourWindow = new AddTourWindow();
+                addTourWindow.ShowDialog();
+            });
         }
 
         public ICommand AddTourCommand { get; set; }
+        public ICommand DeleteTourCommand { get; set; }
+        public ICommand EditTourCommand { get; set; }
         public ObservableCollection<TUYEN> ListTuyen { get => _ListTuyen; set { _ListTuyen = value; OnPropertyChanged(); } }
     }
 }
