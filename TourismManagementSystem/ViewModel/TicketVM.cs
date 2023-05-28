680593bccee1b98fceca01b822c15f2a8d0496e8
Vy Huỳnh Thị Nhật <21522810@ms.uit.edu.vn>
2023-05-28 16:27:03 +0700

    filter+ search + purchase

--- TourismManagementSystem/ViewModel/TicketVM.cs
+++ TourismManagementSystem/ViewModel/TicketVM.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TourismManagementSystem.Model;
+using TourismManagementSystem.View;
 
 namespace TourismManagementSystem.ViewModel
 {
     internal class TicketVM : BaseViewModel
     {
+        public ICommand SwitchWindowCommand { get; set; }
+
+        public ICommand OnlyNumericCommand { get; private set; }
+
+        public ICommand AddDataCommand { get; set; }
+        public ICommand DeleteDataCommand { get; set; }
+        public ICommand UpdateDataCommand { get; set; }
+        private ObservableCollection<string> _status = new ObservableCollection<string>() { "Chưa sử dụng", "Đã sử dụng", "Đã hủy" };
+        public ObservableCollection<string> status { get => _status; set { _status = value; OnPropertyChanged(nameof(status)); } }
+
         private string _ticket;
         public string VE
         {
@@ -68,7 +83,83 @@ namespace TourismManagementSystem.ViewModel
                 OnPropertyChanged(nameof(GIAVE));
             }
         }
-        
 
+
+
+        private ObservableCollection<VE> _ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs);
+        public ObservableCollection<VE> ListVE { get => _ListVE; set { _ListVE = value; OnPropertyChanged(nameof(DataProvider.Ins.DB.VEs)); } }
+
+        private VE _selectedTicket;
+        public VE SelectedTicket
+        {
+            get => _selectedTicket; set
+            {
+                _selectedTicket = value;
+                OnPropertyChanged(nameof(SelectedTicket));
+            }
+        }
+
+
+        public TicketVM()
+        {
+            SwitchWindowCommand = new RelayCommand<object>((p) => {
+
+                return true;
+
+            }, (p) =>
+            { SwitchWindow(p); });
+
+            AddDataCommand = new RelayCommand<object>((p) => {
+                if (MaVE == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }, (p) =>
+            {
+                try
+                {
+                    var temp = new VE()
+                    {
+                        
+                    };
+
+                    DataProvider.Ins.DB.VEs.Add(temp);
+                    DataProvider.Ins.DB.SaveChanges();
+                    ListVE.Add(temp);
+                    //LoadDataGrid();
+
+
+
+                    MessageBox.Show("Đã tạo mới khách hàng thành công");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.InnerException.Message);
+                }
+
+
+            });
+
+
+
+           // OnlyNumericCommand = new RelayCommand<object>((p) =>
+           // {
+           //     return true;
+           // },
+           //(p) => { OnlyNumericExecute(); });
+
+        }
+        private void SwitchWindow(object parameter)
+        {
+            DetailTicketWindow detailTicketWindow= new DetailTicketWindow();
+            detailTicketWindow.Show();
+
+        }
     }
 }
