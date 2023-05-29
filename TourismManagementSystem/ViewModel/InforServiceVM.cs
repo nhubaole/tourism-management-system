2b86445940d8e229210be82b28a7ec1688a266ad
Nhongnhong-0101 <21522423@gm.uit.edu.vn>
2023-05-29 17:51:07 +0700

    Fix UI

--- TourismManagementSystem/ViewModel/InforServiceVM.cs
+++ TourismManagementSystem/ViewModel/InforServiceVM.cs
@@ -4,17 +4,77 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TourismManagementSystem.Model;
+using TourismManagementSystem.UserControls;
 
 namespace TourismManagementSystem.ViewModel
 {
    
     public class InforServiceVM :BaseViewModel
     {
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
         public static bool IsNew; //xác định là tạo mới hay cập nhập
-        public ObservableCollection<String> cmbItems { get; set; }
+
+        private ObservableCollection<string> _FilterItems;
+        public ObservableCollection<string> FilterItems
+        {
+            get { return _FilterItems; }
+            set { _FilterItems = value; OnPropertyChanged(); }
+        }
+        private string _Filter;
+        public string Filter
+        {
+            get { return _Filter; }
+            set
+            {
+                _Filter = value;
+
+                OnPropertyChanged();
+            }
+        }
 
         public InforServiceVM() {
-            cmbItems = new ObservableCollection<String>(new List<string> { "Phương tiện", "Nhà hàng", "Khách sạn", "Dịch vụ khác" });
-        }    
+            FilterItems = new ObservableCollection<String>(new List<string> { "Phương tiện", "Khách sạn", "Nhà hàng", "Dịch vụ khác" });
+
+            Filter = FilterItems.FirstOrDefault();
+            CurrentView = new UCInforTrafffic();
+
+            PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(Filter))
+                {
+                    switch (Filter)
+                    {
+                        case "Phương tiện":
+                            Traffic(null);
+                            break;
+                        case "Nhà hàng":
+                            Restaurant(null);
+                            break;
+                        case "Khách sạn":
+                            Hotel(null);
+                            break;
+                        case "Dịch vụ khác":
+                            OTherService(null);
+                            break;
+                        default:
+                            // Xử lý cho trường hợp khác
+                            break;
+                    }
+                }
+            };
+        }
+        private void Traffic(object obj) => CurrentView = new UCInforTrafffic();
+        private void Restaurant(object obj) => CurrentView = new UCInforRestaurant();
+        private void Hotel(object obj) => CurrentView = new UCInforHotel();
+        private void OTherService(object obj) => CurrentView = new UCInforOtherService();
     }
 }
---
+++ TourismManagementSystem/ViewModel/InforTrafficVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourismManagementSystem.ViewModel
+{
+    internal class InforTrafficVm: BaseViewModel
+    {
+        public InforTrafficVm() {
+        
+        } 
+    }
+}
