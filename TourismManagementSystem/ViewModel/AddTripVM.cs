592f0db7b0f08e101157b1dad6cee7ff4e6b9228
LAPTOP-S74F9L51\Ideapad gaming 3 <univerbachtuoc@gmail.com>
2023-06-01 02:30:12 +0700

    <add> tríp window and add/update trip window

---
+++ TourismManagementSystem/ViewModel/AddTripVM.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using TourismManagementSystem.Model;
+using TourismManagementSystem.View;
+
+namespace TourismManagementSystem.ViewModel
+{
+    internal class AddTripVM: BaseViewModel
+    {
+        private CHUYEN _newTrip;
+        public CHUYEN NewTrip { get => _newTrip; set { _newTrip= value; OnPropertyChanged(); } }
+        public ObservableCollection<LOAICHUYEN> ListLoaiChuyen { get; set; }
+        private string _ToolTipText;
+
+        public ObservableCollection<string> ListTuyen { get; set; }
+        public ObservableCollection<string> ListHDV { get; set; }
+
+        private static CHUYEN _editSelectedChuyen;
+        public static CHUYEN EditSelectedChuyen { get => _editSelectedChuyen; set { _editSelectedChuyen = value; } }
+        private static int _IsEdit = 0;
+
+        private string _Title;
+        private string _BtnText;
+
+        public AddTripVM()
+        {
+            ToolTipText = "Chưa nhập đủ thông tin";
+            ListLoaiChuyen = new ObservableCollection<LOAICHUYEN>(DataProvider.Ins.DB.LOAICHUYENs);
+            ListTuyen = new ObservableCollection<string>(DataProvider.Ins.DB.TUYENs.Select(t => t.MATUYEN));
+            ListHDV = new ObservableCollection<string>(DataProvider.Ins.DB.HUONGDANVIENs.Select(t => t.MAHDV));
+            if (IsEdit == 0)
+            {
+                Title = "Thêm mới cuyến du lịch";
+                BtnText = "Thêm chuyến du lịch";
+                NewTrip = new CHUYEN();
+                Random random = new Random();
+                NewTrip.MACHUYEN = random.Next(0, 1000).ToString();
+            }
+            else
+            {
+                Title = "Cập nhật chuyến du lịch";
+                BtnText = "Cập nhật chuyến du lịch";
+                NewTrip = EditSelectedChuyen;
+            }
+
+
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(NewTrip.MACHUYEN) || NewTrip.MATUYEN == null || NewTrip.LOAICHUYEN == null || NewTrip.HUONGDANVIENs.Count == 0 || NewTrip.TGBATDAU == null || NewTrip.GIAVE == null || NewTrip.TGKETTHUC == null || NewTrip.SLTOITHIEU == null || NewTrip.SLTHUCTE == null)
+                {
+                    ToolTipText = "Vui lòng nhập đủ các trường thông tin bắt buộc";
+                    return false;
+                }
+                return true;
+            }, (p) =>
+            {
+                var addTripWindow = p as Window;
+                if (addTripWindow != null)
+                {
+                    if (NewTrip.TGBATDAU > NewTrip.TGKETTHUC)
+                    {
+                        MessageBox.Show("Thời gian không hợp lệ");
+                        return;
+                    }
+                    if (IsEdit == 0)
+                    {
+                        NewTrip.MALOAI = DataProvider.Ins.DB.LOAICHUYENs.Where(t => t.TENLOAI.Equals(NewTrip.LOAICHUYEN)).ToString();
+                        DataProvider.Ins.DB.CHUYENs.Add(NewTrip);
+                        DataProvider.Ins.DB.SaveChanges();
+                        MessageBox.Show("Thêm mới chuyến thành công!");
+                    }
+                    else
+                    {
+                        var itemToUpdate = DataProvider.Ins.DB.CHUYENs.FirstOrDefault(item => item.MACHUYEN == NewTrip.MACHUYEN);
+                        if (itemToUpdate != null)
+                        {
+                            NewTrip.MALOAI = DataProvider.Ins.DB.LOAICHUYENs.Where(t => t.TENLOAI.Equals(NewTrip.LOAICHUYEN)).ToString();
+                            itemToUpdate = NewTrip;
+                            DataProvider.Ins.DB.SaveChanges();
+                            MessageBox.Show("Cập nhật thành công!");
+                        }
+                    }
+                    addTripWindow.Close();
+                }
+            });
+            AddHDV = new RelayCommand<string>((p) => {
+                if (SelectedHDV != null)
+                {
+                    string HDV = SelectedHDV;
+                    NewTrip.HUONGDANVIENs.Add((HUONGDANVIEN)DataProvider.Ins.DB.HUONGDANVIENs.Where(t => t.MAHDV.Equals(HDV)));
+                }
+            });
+        }
+
+        private string _selectedHDV;
+        public string SelectedHDV
+        {
+            get { return _selectedHDV; }
+            set
+            {
+                _selectedHDV = value;
+                OnPropertyChanged(); // Implement INotifyPropertyChanged to notify the UI of property changes
+            }
+        }
+        public ICommand AddCommand { get; set; }
+
+        public ICommand AddHDV { get; set; }
+        public ICommand ViewHDV { get; set; }
+
+        public string ToolTipText { get => _ToolTipText; set { _ToolTipText = value; OnPropertyChanged(); } }
+        public static int IsEdit { get => _IsEdit; set => _IsEdit = value; }
+        public string Title { get => _Title; set => _Title = value; }
+        public string BtnText { get => _BtnText; set => _BtnText = value; }
+    }
+}
--- TourismManagementSystem/ViewModel/TripVM.cs
+++ TourismManagementSystem/ViewModel/TripVM.cs
@@ -1,12 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using TourismManagementSystem.UserControls;
+using TourismManagementSystem.Model;
+using System.Windows.Input;
+using TourismManagementSystem.View;
+using System.Windows;
 
 namespace TourismManagementSystem.ViewModel
 {
     internal class TripVM : BaseViewModel
     {
+
+        public ICommand AddTripCommand { get; set; }
+        public ICommand DeleteTripCommand { get; set; }
+        public ICommand EditTripCommand { get; set; }
+        public ICommand ViewTripCommand { get; set; }
+
+        private ObservableCollection<CHUYEN> _ChuyenList;
+        public ObservableCollection<CHUYEN> ChuyenList { get => _ChuyenList; set { _ChuyenList = value; OnPropertyChanged(); } }
+        private string _Search;
+        public string Search
+        {
+            get => _Search;
+            set
+            {
+                _Search = value;
+                if (SelectedFilter == FilterCbItems[0])
+                {
+                    ChuyenList = new ObservableCollection<CHUYEN>(DataProvider.Ins.DB.CHUYENs.Where(t => t.MACHUYEN.Contains(Search)));
+                }
+                else if (SelectedFilter == FilterCbItems[1])
+                {
+                    ChuyenList = new ObservableCollection<CHUYEN>(DataProvider.Ins.DB.CHUYENs.Where(t => t.MATUYEN.Contains(Search)));
+                }
+                else if (SelectedFilter == FilterCbItems[2])
+                {
+                    ChuyenList = new ObservableCollection<CHUYEN>(DataProvider.Ins.DB.CHUYENs.Where(t => t.TGBATDAU.Value.Day.Equals(Search)));
+                }
+                else if (SelectedFilter == FilterCbItems[3])
+                {
+                    ChuyenList = new ObservableCollection<CHUYEN>(DataProvider.Ins.DB.CHUYENs.Where(t => t.LOAICHUYEN.TENLOAI.Contains(Search)));
+                }
+
+                OnPropertyChanged();
+            }
+        }
+        private string selectedFilter;
+        public string SelectedFilter { get => selectedFilter; set { selectedFilter = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<string> _FilterCbItems = new ObservableCollection<string>() { "Mã chuyến", "Mã tuyến", "Thời gian bắt đầu", "Loại chuyến" };
+        public ObservableCollection<string> FilterCbItems { get => _FilterCbItems; set => _FilterCbItems = value; }
+
+        private CHUYEN _selectedChuyen;
+        public CHUYEN SelectedChuyen
+        {
+            get => _selectedChuyen;
+            set
+            {
+                _selectedChuyen = value;
+                OnPropertyChanged();
+                if (SelectedChuyen != null)
+                {
+                    MaChuyen = SelectedChuyen.MACHUYEN;
+                    MaTuyen = SelectedChuyen.MATUYEN;
+                    Tgbd = SelectedChuyen.TGBATDAU;
+                    LOAICHUYEN = SelectedChuyen.LOAICHUYEN;
+                }
+            }
+        }
+
+        private string _MaChuyen;
+        public string MaChuyen { get => _MaChuyen; set { _MaChuyen = value; OnPropertyChanged(); } }
+
+        private string _MaTuyen;
+        public string MaTuyen { get => _MaTuyen; set { _MaTuyen = value; OnPropertyChanged(); } }
+
+
+        private DateTime? _Tgbd;
+        public DateTime? Tgbd { get => _Tgbd; set { _Tgbd = value; OnPropertyChanged(); } }
+
+
+        private LOAICHUYEN _LOAICHUYEN;
+        public LOAICHUYEN LOAICHUYEN { get => _LOAICHUYEN; set { _LOAICHUYEN = value; OnPropertyChanged(); } }
+
+        public TripVM()
+        {
+            ChuyenList = new ObservableCollection<CHUYEN>(DataProvider.Ins.DB.CHUYENs);
+            AddTripCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                AddTripVM.IsEdit = 0;
+                AddTripWindow addTourWindow = new AddTripWindow();              
+                addTourWindow.ShowDialog();
+                ChuyenList = new ObservableCollection<CHUYEN>(DataProvider.Ins.DB.CHUYENs);
+            });
+            DeleteTripCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                CHUYEN selectedChuyen = p as CHUYEN;
+                if (MessageBox.Show("Bạn có muốn xóa chuyến du lịch này?", "Xác nhận", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    DataProvider.Ins.DB.CHUYENs.Remove(selectedChuyen);
+                    DataProvider.Ins.DB.SaveChanges();
+                    ChuyenList = new ObservableCollection<CHUYEN>(DataProvider.Ins.DB.CHUYENs);
+                    MessageBox.Show("Xóa chuyến du lịch thành công");
+                }
+            });
+            EditTripCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                CHUYEN selectedChuyen = p as CHUYEN;
+                AddTripVM.EditSelectedChuyen = selectedChuyen;
+                AddTripVM.IsEdit = 1;
+                AddTripWindow addTripWindow = new AddTripWindow();
+                addTripWindow.ShowDialog();
+            });
+            ViewTripCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                CHUYEN selectedChuyen = p as CHUYEN;
+                //TourDetailsVM.SelectedTour = selectedChuyen;
+                TripDetailsWindow tripDetailWindow = new TripDetailsWindow();
+                tripDetailWindow.ShowDialog();
+            });
+        }
+
     }
 }
