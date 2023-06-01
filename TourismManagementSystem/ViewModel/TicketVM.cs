21288f922cfecc4d41faa4ea28c0b7b06cf8a609
nhubaole <okamisameko7123@gmail.com>
2023-06-01 19:04:43 +0700

    complete ticket management features

--- TourismManagementSystem/ViewModel/TicketVM.cs
+++ TourismManagementSystem/ViewModel/TicketVM.cs
@@ -13,153 +13,70 @@ namespace TourismManagementSystem.ViewModel
 {
     internal class TicketVM : BaseViewModel
     {
-        public ICommand ShowTicketDetailCommand { get; set; }
-
-        public ICommand UpdateDataCommand { get; set; }
-      
-        private ObservableCollection<string> _filter = new ObservableCollection<string>() { "Mã phiếu", "Mã vé", "Mã khách hàng" };
-        public ObservableCollection<string> filter { get => _filter; set { _filter = value; OnPropertyChanged(nameof(filter)); } }
-       
-
-        private string _ticket;
-        public string VE
-        {
-            get => _ticket;
-            set
-            {
-                _ticket= value;
-                OnPropertyChanged(nameof(VE));
-            }
-        }
-        private string MaVE;
-        public string MAVE
-        {
-            get =>  MaVE;
-            set
-            {
-                MaVE = value;
-                OnPropertyChanged(nameof(MAVE));
-            }
-        }
-        private string MaPHIEU;
-        public string MAPHIEU
-        {
-            get => MaPHIEU;
-            set
-            {
-                MaPHIEU = value;
-                OnPropertyChanged(nameof(MAPHIEU));
-            }
-        }
-        private string TrangThai;
-        public string TRANGTHAI
-        {
-            get => TrangThai;
-            set
-            {
-                TrangThai = value;
-                OnPropertyChanged(nameof(TRANGTHAI));
-            }
-        }
-        private DateTime NgayBan;
-        public DateTime NGAYBAN
-        {
-            get => NgayBan;
-            set
-            {
-                NgayBan = value;
-                OnPropertyChanged(nameof(NGAYBAN));
-            }
-        }
-        private int GiaVE;
-        public int GIAVE
-        {
-            get => GiaVE;
-            set
-            {
-                GiaVE= value;
-                OnPropertyChanged(nameof(GIAVE));
-            }
-        }
-
-
-
+        private string _SelectedFilter;
+        private string _SearchText;
+        private ObservableCollection<string> _FilterCbItems = new ObservableCollection<string>() {  "Mã vé", "Mã hành khách", "Mã phiếu", "Ngày bán", "Trạng thái" };
         private ObservableCollection<VE> _ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs);
-        public ObservableCollection<VE> ListVE { get => _ListVE; set { _ListVE = value; OnPropertyChanged(nameof(DataProvider.Ins.DB.VEs)); } }
-
-        private VE _selectedTicket;
-        public VE SelectedTicket
-        {
-            get => _selectedTicket; set
-            {
-                _selectedTicket = value;
-                OnPropertyChanged(nameof(SelectedTicket));
-            }
-        }
-
-        private ObservableCollection<string> _status = new ObservableCollection<string>() { "Mã phiếu", "Mã vé", "Mã khách hàng" };
-        public ObservableCollection<string> status { get => _status; set { _status = value; OnPropertyChanged(nameof(status)); } }
-
-
+        private ObservableCollection<string> _ListStatus = new ObservableCollection<string>() { "Chưa sử dụng", "Đã sử dụng", "Đã hủy" };
 
         public TicketVM()
         {
-            status= new ObservableCollection<string>();
-            ShowTicketDetailCommand = new RelayCommand<object>((p) => {
-
-                return true;
-
-            }, (p) =>
-            { SwitchWindow(p); });
-
-            UpdateDataCommand = new RelayCommand<object>((p) => {
-                if (MaVE == null)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }, (p) =>
-            {
-                try
-                {
-                    var temp = new VE()
-                    {
-                        TRANGTHAI = TRANGTHAI,
-                        NGAYBAN = NGAYBAN
-                    };
-
-                    DataProvider.Ins.DB.SaveChanges();
-                    LoadDataGrid();
-                    MessageBox.Show("Đã thay đổi thông tin vé thành công");
-
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("An error occurred: " + ex.Message);
-                }
-
-
+            SelectedFilter = FilterCbItems.First();
+            ShowTicketDetailCommand = new RelayCommand<object>((p) => true,
+            (p) => {
+                DetailTicketWindow detailTicketWindow = new DetailTicketWindow();
+                detailTicketWindow.Show();
             });
 
-
-
-       
-
+            UpdateStatusCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                VE selectedVe = p as VE;
+                var itemToUpdate = DataProvider.Ins.DB.VEs.FirstOrDefault(item => item.MAVE == selectedVe.MAVE);
+                if (itemToUpdate != null)
+                {
+                    itemToUpdate = selectedVe;
+                    DataProvider.Ins.DB.SaveChanges();
+                    MessageBox.Show("Cập nhật trạng thái vé thành công!");
+                }
+            });
         }
-        private void SwitchWindow(object parameter)
-        {
-            DetailTicketWindow detailTicketWindow= new DetailTicketWindow();
-            detailTicketWindow.Show();
 
-        }
-        private void LoadDataGrid()
+
+        public ICommand ShowTicketDetailCommand { get; set; }
+        public ICommand UpdateStatusCommand { get; set; }
+
+        public ObservableCollection<string> ListStatus { get => _ListStatus; set => _ListStatus = value; }
+        public ObservableCollection<VE> ListVE { get => _ListVE; set { _ListVE = value; OnPropertyChanged(); } }
+        public ObservableCollection<string> FilterCbItems { get => _FilterCbItems; set { _FilterCbItems = value; OnPropertyChanged(); } }
+        public string SelectedFilter { get => _SelectedFilter; set { _SelectedFilter = value; OnPropertyChanged(); } }
+        public string SearchText
         {
-            ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs);
-            OnPropertyChanged(nameof(ListVE));
+            get => _SearchText;
+            set
+            {
+                _SearchText = value;
+                if (SelectedFilter == FilterCbItems[0])
+                {
+                    ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs.Where(t => t.MAVE.Contains(SearchText)));
+                }
+                else if (SelectedFilter == FilterCbItems[1])
+                {
+                    ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs.Where(t => t.MAHK.Contains(SearchText)));
+                }
+                else if (SelectedFilter == FilterCbItems[2])
+                {
+                    ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs.Where(t => t.MAPHIEU.Contains(SearchText)));
+                }
+                else if (SelectedFilter == FilterCbItems[3])
+                {
+                    ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs.Where(t => t.NGAYBAN.ToString().Contains(SearchText)));
+                }
+                else if (SelectedFilter == FilterCbItems[4])
+                {
+                    ListVE = new ObservableCollection<VE>(DataProvider.Ins.DB.VEs.Where(t => t.TRANGTHAI.Contains(SearchText)));
+                }
+                OnPropertyChanged();
+            }
         }
     }
 }
