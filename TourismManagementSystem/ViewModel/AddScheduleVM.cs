aaa61ac6512f0a0b4d65f9c9e319f04a6eb36669
nhubaole <okamisameko7123@gmail.com>
2023-06-03 16:45:02 +0700

    update: generate ID

--- TourismManagementSystem/ViewModel/AddScheduleVM.cs
+++ TourismManagementSystem/ViewModel/AddScheduleVM.cs
@@ -151,8 +151,20 @@ namespace TourismManagementSystem.ViewModel
                     if(IsEdit == 0)
                     {
                         LICHTRINH newLichTrinh = new LICHTRINH();
-                        Random random = new Random();
-                        newLichTrinh.MALT = random.Next(1, 1000).ToString();
+                        string formattedID;
+                        ObservableCollection<LICHTRINH> ListLT = new ObservableCollection<LICHTRINH>(DataProvider.Ins.DB.LICHTRINHs);
+                        if (ListLT.Count() == 0)
+                        {
+                            formattedID = string.Format("LT{0:D6}", 1);
+                        }
+                        else
+                        {
+                            string lastID = ListLT.Last().MALT;
+                            int previousNumber = int.Parse(lastID.Substring(2));
+                            int nextNumber = previousNumber + 1;
+                            formattedID = string.Format("LT{0:D6}", nextNumber);
+                        }
+                        newLichTrinh.MALT = formattedID;
                         newLichTrinh.DIADIEM = DDDi;
                         newLichTrinh.DIADIEM1 = DDDen;
                         foreach (var item in PhuongTienBoxes)
