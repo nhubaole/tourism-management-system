47bdecdc9e142dc35674a4e1b5d817cf16cfe9c4
Nhongnhong-0101 <21522423@gm.uit.edu.vn>
2023-11-08 20:56:19 +0700

    test class DVK, CHUYEN, DIADIEM

--- TestDoAn/CHUYENTest.cs
+++ TestDoAn/CHUYENTest.cs
@@ -77,7 +77,6 @@ namespace TestDoAn
         }
 
 
-
         [TestMethod]
         public void TestGIAVEProperty()
         {
@@ -116,6 +115,61 @@ namespace TestDoAn
             // Assert
             Assert.AreEqual("T0000001", chuyen.MATUYEN);
         }
+        [TestMethod]
+        public void TestLOAICHUYENProperty()
+        {
+            // Arrange
+            var chuyen = new CHUYEN();
+            var loaiChuyen = new LOAICHUYEN();
+
+            // Act
+            chuyen.LOAICHUYEN = loaiChuyen;
+
+            // Assert
+            Assert.AreEqual(loaiChuyen, chuyen.LOAICHUYEN);
+        }
+        [TestMethod]
+        public void TestTUYENProperty()
+        {
+            // Arrange
+            var chuyen = new CHUYEN();
+            var tuyen = new TUYEN();
+
+            // Act
+            chuyen.TUYEN = tuyen;
+
+            // Assert
+            Assert.AreEqual(tuyen, chuyen.TUYEN);
+        }
+
+        [TestMethod]
+        public void TestPHIEUDATCHOesProperty()
+        {
+            // Arrange
+            var chuyen = new CHUYEN();
+            var phieu = new PHIEUDATCHO();
+
+            // Act
+            chuyen.PHIEUDATCHOes.Add(phieu);
+
+            // Assert
+            Assert.IsTrue(chuyen.PHIEUDATCHOes.Contains(phieu));
+        }
+
+        [TestMethod]
+        public void TestHUONGDANVIENsProperty()
+        {
+            // Arrange
+            var chuyen = new CHUYEN();
+            var huongDanVien = new HUONGDANVIEN();
+
+            // Act
+            chuyen.HUONGDANVIENs.Add(huongDanVien);
+
+            // Assert
+            Assert.IsTrue(chuyen.HUONGDANVIENs.Contains(huongDanVien));
+        }
+
 
         [TestMethod]
         public void TestTHANHCONGroperty()
---
+++ TestDoAn/DIADIEMTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourismManagementSystem.Model;
+
+namespace TestDoAn
+{
+    [TestClass]
+    public class DIADIEMTest
+    {
+        [TestMethod]
+        public void TestMaDDProperty()
+        {
+            // Arrange
+            var d = new DIADIEM();
+
+            // Act
+            d.MADD = "DD00000001";
+
+            // Assert
+            Assert.AreEqual("DD00000001", d.MADD);
+        }
+        [TestMethod]
+        public void TestTENDDProperty()
+        {
+            var d = new DIADIEM();
+
+            d.TENDD = "BMT";
+
+            Assert.AreEqual("BMT", d.TENDD);
+        }
+
+        [TestMethod]
+        public void TestDIACHIDDProperty()
+        {
+            var d = new DIADIEM();
+
+            d.DIACHI = "01 Hồ Tùng Mậu";
+
+            Assert.AreEqual("01 Hồ Tùng Mậu", d.DIACHI);
+
+        }
+
+        [TestMethod]
+        public void TestMOTAProperty()
+        {
+            var d = new DIADIEM();
+
+            d.MOTA = "Phù hợp 50 người";
+
+            Assert.AreEqual("Phù hợp 50 người", d.MOTA);
+        }
+
+        [TestMethod]    
+        public void TestLICHTRINHsProperty() 
+        {
+            var d = new DIADIEM();
+            var lich = new LICHTRINH();
+            d.LICHTRINHs.Add(lich);
+
+            Assert.IsTrue(d.LICHTRINHs.Contains(lich));
+        }
+
+        [TestMethod]
+        public void TestLICHTRINH1sProperty() 
+        {
+            var d = new DIADIEM();
+            var lich1 = new LICHTRINH();
+            d.LICHTRINHs1.Add(lich1);
+
+            Assert.IsTrue(d.LICHTRINHs1.Contains(lich1));
+        }
+
+        [TestMethod]
+        public void TestTUYENsValue()
+        {
+            var d = new DIADIEM();
+
+            var tuyen = new TUYEN();    
+
+            d.TUYENs.Add(tuyen);
+
+            Assert.IsTrue(d.TUYENs.Contains(tuyen));
+
+        }
+    }
+}
