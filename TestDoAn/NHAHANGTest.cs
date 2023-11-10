c9c2efb482fe2348d4f09a39125fab9c53ef14af
Hà Mai Anh <114716577+AnhHa03@users.noreply.github.com>
2023-11-10 19:40:49 +0700

    Model Test

---
+++ TestDoAn/NHAHANGTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourismManagementSystem.Model;
+
+namespace TestDoAn
+{
+    [TestClass]
+    public class NHAHANGTest
+    {
+        [TestMethod]
+        public void TestMANHAHANGProperty()
+        {
+            var nh = new NHAHANG();
+            string ma = "NH000001";
+            nh.MANH = ma;
+
+            Assert.AreEqual(ma, nh.MANH);
+        }
+
+        [TestMethod]
+        public void TestTENNHAHANGProperty()
+        {
+            var nh = new NHAHANG();
+            string ten = "Nhà hàng Vịnh Hạ Long";
+            nh.TENNH = ten;
+
+            Assert.AreEqual(ten, nh.TENNH);
+        }
+
+        [TestMethod]
+        public void TestSDTNHAHANGProperty()
+        {
+            var nh = new NHAHANG();
+            string sdt = "0344004890";
+            nh.SDT = sdt;
+
+            Assert.AreEqual(sdt, nh.SDT);
+        }
+
+        [TestMethod]
+        public void TestMOTANHAHANGProperty()
+        {
+            var nh = new NHAHANG();
+            string mt = "5 sao";
+            nh.MOTA = mt;
+
+            Assert.AreEqual(mt, nh.MOTA);
+        }
+
+        [TestMethod]
+        public void TestLICHTRINHsProperty()
+        {
+            var nh = new NHAHANG();
+            var lich = new LICHTRINH();
+
+            nh.LICHTRINHs.Add(lich);
+
+            Assert.IsTrue(nh.LICHTRINHs.Contains(lich));
+
+        }
+    }
+}
---
+++ TestDoAn/THONGTINTHANHTOANTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourismManagementSystem.Model;
+
+namespace TestDoAn
+{
+    [TestClass]
+    public class THONGTINTHANHTOANTest
+    {
+        [TestMethod]
+        public void TestMATTProperty()
+        {
+            var tttt = new THONGTINTHANHTOAN();
+            string ma = "TT000001";
+            tttt.MATT = ma;
+
+            Assert.AreEqual(ma, tttt.MATT);
+        }
+
+        [TestMethod]
+        public void TestMAPHIEUProperty()
+        {
+            var tttt = new THONGTINTHANHTOAN();
+            string maphieu = "PH000001";
+            tttt.MAPHIEU = maphieu;
+
+            Assert.AreEqual(maphieu, tttt.MAPHIEU);
+        }
+
+        [TestMethod]
+        public void TestSOTIENProperty()
+        {
+            var tttt = new THONGTINTHANHTOAN();
+            int st = 4000000;
+            tttt.SOTIEN = st;
+
+            Assert.AreEqual(st, tttt.SOTIEN);
+        }
+
+        [TestMethod]
+        public void TestTHOIGIANProperty()
+        {
+            var tttt = new THONGTINTHANHTOAN();
+            DateTime tg = DateTime.Now;
+            tttt.THOIGIAN = tg;
+
+            Assert.AreEqual(tg, tttt.THOIGIAN);
+        }
+
+        [TestMethod]
+        public void TestTRANGTHAIProperty()
+        {
+            var tttt = new THONGTINTHANHTOAN();
+            string tt = "SOLD";
+            tttt.TRANGTHAI = tt;
+
+            Assert.AreEqual(tt, tttt.TRANGTHAI);
+        }
+
+        [TestMethod]
+        public void TestPHUONGTHUCProperty()
+        {
+            var tttt = new THONGTINTHANHTOAN();
+            string pt = "Tiền mặt";
+            tttt.PHUONGTHUC = pt;
+
+            Assert.AreEqual(pt, tttt.PHUONGTHUC);
+        }
+
+        [TestMethod]
+        public void TestPHIEUDATCHOProperty()
+        {
+            var tttt = new THONGTINTHANHTOAN();
+            PHIEUDATCHO pdc = new PHIEUDATCHO();
+            tttt.PHIEUDATCHO = pdc;
+
+            Assert.AreEqual(pdc, tttt.PHIEUDATCHO);
+        }
+    }
+}
